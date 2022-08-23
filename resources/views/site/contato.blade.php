41a6a1a94440a19a05be230aa73e512e75d1566d
PedroC0de <pedro.augus.lima3@gmail.com>
2022-08-23 11:56:49 -0300

    adicionando layouts para facilitar o front do projeto

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,20 +1,7 @@
 @extends('site.layouts.basico')
-
+@section('titulo', 'contato')
 @section('conteudo')
-        <div class="topo">
 
-            <div class="logo">
-                <img src="img/logo.png">
-            </div>
-
-            <div class="menu">
-                <ul>
-                    <li><a href="{{ route('site.index') }}">Principal</a></li>
-                    <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-                    <li><a href="{{ route('site.contato') }}">Contato</a></li>
-                </ul>
-            </div>
-        </div>
 
         <div class="conteudo-pagina">
             <div class="titulo-pagina">
