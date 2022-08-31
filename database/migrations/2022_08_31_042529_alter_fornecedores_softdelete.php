299289035f3f6e486e83153d25b0fb75a9104e15
PedroC0de <pedro.augus.lima3@gmail.com>
2022-08-31 01:28:46 -0300

    adicionando o soft delete no model Fornecedor e adicionando nova migrate para o softdelete

--- database/migrations/2022_08_31_042529_alter_fornecedores_softdelete.php
+++ database/migrations/2022_08_31_042529_alter_fornecedores_softdelete.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFornecedoresTable extends Migration
+class AlterFornecedoresSoftdelete extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateFornecedoresTable extends Migration
      */
     public function up()
     {
-        Schema::create('fornecedores', function (Blueprint $table) {
-            $table->id();
-            $table->string('nome', 50);
-            $table->timestamps();
-            
-        });
+        Schema::table('fornecedores', function (Blueprint $table) {
+        $table->softDeletes();
+    });
     }
 
     /**
@@ -28,6 +25,8 @@ class CreateFornecedoresTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('fornecedores');
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
     }
 }
