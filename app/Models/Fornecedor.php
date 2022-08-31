d7f97f44f70e145ec7ef4a25c569f08e4f7ca1e9
PedroC0de <pedro.augus.lima3@gmail.com>
2022-08-31 01:23:06 -0300

    adicionando o soft delete no model Fornecedor

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
+    use SoftDeletes;
     protected $table="fornecedores";
     protected $fillable=["nome", "site", "uf", "email"];
     use HasFactory;
