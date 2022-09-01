60b0d2b527563c9648f5c12504cc5a33ac8f0a48
PedroC0de <pedro.augus.lima3@gmail.com>
2022-09-01 12:00:45 -0300

    adicinando metodo para salvar dados do formulario contato no banco

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\Models\SiteContato;
 
 class ContatoController extends Controller {
 
- public function contato(){
-        var_dump($_POST);
+ public function contato(Request $request){
+        
+
+    $contato = new SiteContato();
+    $contato->nome = $request->input('nome');
+    $contato->telefone = $request->input('telefone');
+    $contato->email = $request->input('email');
+    $contato->motivo_contato = $request->input('motivo_contato');
+    $contato->mensagem = $request->input('mensagem');
+
+    $contato->save();
         return(view('site.contato'));
     }
 }
